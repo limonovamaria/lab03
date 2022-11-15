c5d319081ca96e882a3e5505468107ee1acaa64f
Лимонова Мария <Marusia.limonova@gmail.com>
2022-11-15 13:42:07 +0500

    puzirek_nf

--- main.py
+++ main.py
@@ -3,14 +3,21 @@ n = int(input("n="))
 a = np.zeros(n)
 for i in range (0,n):
     a[i] = int(input())
-
-for i in range(0,n):
-    for j in range(i,n):
-        if (a[j]<a[i]):
-            b = a[i]
-            a[i] = a[j]
-            a[j] = b
-
+direction = int(input("введите 1, если сортировать по возрастанию, иначе 0->"))
+if (direction ==1):
+    for i in range(0,n):
+        for j in range(i,n):
+            if (a[j]<a[i]):
+                b = a[i]
+                a[i] = a[j]
+                a[j] = b
+else :
+    for i in range(0,n):
+        for j in range(i,n):
+            if (a[j]>a[i]):
+                b = a[i]
+                a[i] = a[j]
+                a[j] = b
 
 for i in range (0,n):
     print(a[i])
